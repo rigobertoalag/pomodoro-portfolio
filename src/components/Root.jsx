1b07295aa46c9d194128cf52dbf44b51b9bc8e7b
rigobertoalag <a20150338@utem.edu.mx>
2021-12-08 19:00:20 -0600

    Minimal changes

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -8,12 +8,12 @@ export default function Root(){
         <div className="mt-8 sm:w-full md:w-full lg:full flex flex-col items-center">
             <p className="font-bold text-3xl md:text-5xl ">Tecnica pomodoro</p>
             <Timer />
-            <button className="mb-8" onClick={()=>setOpen(true)}>Mas informacion de Pomodoro</button>
+            <button className="mb-4" onClick={()=>setOpen(true)}>Mas informacion de Pomodoro</button>
             {
                 open ? <Modal /> : null
             }
             <div className="bg-red-500 w-full h-11 flex flex-row justify-center items-center">
-                <p className="text-white font-light text-sm">Coded by Rigoberto Alcantar</p>
+                <p className="text-blue-600 font-light text-sm">Coded by Rigoberto Alcantar</p>
             </div>
         </div>
     )
